50c22b3e3631614f7bf504e6ab95c2b6c4844f22
menghanzhao <menghan1@ualberta.ca>
2021-10-07 13:42:01 -0600

    adding map in jupyter notebook

---
+++ wsgi.py
@@ -0,0 +1,10 @@
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+from dash.dependencies import Input, Output, State
+
+import pandas as pd
+import datetime as dt
+from datetime import date, datetime
+import geojson
